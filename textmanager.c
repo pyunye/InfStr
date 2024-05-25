f453d39b0c40518b08f8262b5ba6357bcd565658
pyunye <dpwns2318@naver.com>
2024-05-25 20:51:34 +0900

    Update textmanager.c

--- textmanager.c
+++ textmanager.c
@@ -23,10 +23,10 @@ void scoreInput(char *username, int score) {
     fclose(f);
 }
 
-void scoreOutput() {
-	 FILE *f;
-    Player players[MAX_SIZE];
-    int num_players = 0;
+Player *scoreOutput(int *num_players) {
+    FILE *f;
+    Player *players = malloc(MAX_SIZE * sizeof(Player)); 
+    *num_players = 0;
 
     // 파일 열기
     if ((f = fopen("score.txt", "r")) == NULL) {
@@ -35,19 +35,42 @@ void scoreOutput() {
     }
 
     // 파일에서 데이터 읽기
-    while (fscanf(f, "%s %d", players[num_players].username, &players[num_players].score) != EOF) {
-        num_players++;
+    while (fscanf(f, "%s %d", players[*num_players].username, &players[*num_players].score) != EOF) {
+        (*num_players)++;
     }
 
     // 소팅
-    qsort(players, num_players, sizeof(Player), compare);
-
-    // 소팅된 데이터 출력
-    printf("Ranking:\n");
-    for (int i = 0; i < num_players; i++) {
-        printf("%s - %d\n", players[i].username, players[i].score);
-    }
+    qsort(players, *num_players, sizeof(Player), compare);
 
     fclose(f); // 파일 닫기
+    
+    players[*num_players].username[0] = '\0';
+    players[*num_players].score = 0;
+
+    return players;
 }
 
+int main() {
+    char username[50];
+    int score;
+    int num_players;
+    
+    printf("유저 이름을 입력하세요: ");
+    scanf("%s", username);
+    printf("점수를 입력하세요: ");
+    scanf("%d", &score);
+    
+    scoreInput(username, score);
+    
+    Player *players = scoreOutput(&num_players); 
+    
+    printf("Ranking:\n");
+    for (int i = 0; i < num_players; i++) {
+        printf("%s : %d\n", players[i].username, players[i].score);
+    }
+    
+    // 동적 할당된 메모리 해제
+    free(players);
+    
+    return 0;
+}
