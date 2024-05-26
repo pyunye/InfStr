c911318604564b7990804c2865101ff1a769d1bb
pyunye <pyunye@github.com>
2024-05-26 14:41:31 +0900

    help

--- help.c
+++ help.c
@@ -5,5 +5,8 @@
 #include<unistd.h>
 
 void help(){
-	
+	char helpText[512] = "<<<<<	Help : Infinite Stair	   >>>>>\n\n* Press left or right to move upstair!\n* If you don’t press any button for a long time or press a wrong button, game is over.\n* If you get a coin, you can press the button slowly.\n\n> Back key 'b'\n";
+	while(getKey() != 'b'){
+		printHelp(helpText);
+	}
 }
