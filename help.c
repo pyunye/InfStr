a29de6441efb5ee092d4aa93a58c21517eab31ae
pyunye <pyunye@github.com>
2024-05-28 21:39:10 +0900

    help.c finite

--- help.c
+++ help.c
@@ -1,48 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#include<curses.h>
-#include<unistd.h>
-#define BACKKEY "> back key 'b'\n\n"
-typedef struct {
-    char username[50];
-    int score;
-} Player;
-
-typedef struct {
-	Player* members;
-	int size;
-} Players;
-
-void printHelp(char []);
 void help(){
 	char helpText[512] = "<<<<<	Help : Infinite Stair	   >>>>>\n\n* Press left or right to move upstair!\n* If you don’t press any button for a long time or press a wrong button, game is over.\n* If you get a coin, you can press the button slowly.\n\n";
-	while(getch() != 'b'){
+	while(getKey() != 'b'){
 		printHelp(helpText);
 	}
 }
 
-void printHelp(char text[]){
-	initscr();
-	noecho();
-	clear();
-	addstr(text);
-	addstr(BACKKEY);
-	refresh();
-}
-
-void printRank(Players unit){
-	initscr();
-	noecho();
-	clear();
-	char playersInfo[256];
-	int i;
-	while(i < unit.size && i < 10){
-		sprintf(playersInfo, " Rank %d : %d(%s)\n", i+1, unit.members[i].score, unit.members[i].username);
-		addstr(playersInfo);
-		i++;
-	}
-	addstr("\n");
-	addstr(BACKKEY);
-	refresh();
-}
