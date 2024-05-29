b5516681afa25da900cac44115cc3070f7a95a2a
dltjddnr <wook8321@naver.com>
2024-05-29 14:54:44 +0900

    change score

--- stairs.h
+++ stairs.h
@@ -1,4 +1,13 @@
+typedef struct Stair{
+	int dir;
+	struct Stair* next;
+} Stair;
+typedef struct{
+	Stair* next;
+	Stair* last;
+} Header;
 void createNewStair();
 void init();
 void clearQueue();
 int isCorrectDirection(int key);
+int* getStairsDir();
